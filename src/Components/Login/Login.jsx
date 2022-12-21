935b853d9bb3902b7a7ea4b570afb5d4f72282b5
AbaniwoM <michaelabaniwo@gmail.com>
2022-12-21 21:16:18 +0100

    update

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="head">Nest</div>
+        <div className="content">
+            <div className="left">
+                <h1>Welcome back</h1>
+                <h4>Login to your account</h4>
+                <div>
+                    
+                </div>
+            </div>
+            <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
