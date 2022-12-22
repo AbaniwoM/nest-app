cc1288f91aa3d3a10e9c8c6753976eb95a5738fb
mo-renike <wilfriedakpe3@gmail.com>
2022-12-22 03:05:55 +0100

    steps

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,9 +5,16 @@ import './Home.scss'
 const Home = () => {
     return (
         <div className='home'>
-            <Button title='Pay with Card' onClick={() => console.log('clicked')} />
-            <Button title='Pay with Bank Transfer' onClick={() => console.log('clicked')} />
-            <Button2 className="bg-bk" title='Pay with NestPay' onClick={() => console.log('clicked')} />
+            <div id="step1">
+                <Button title='Pay with Card' onClick={() => console.log('clicked')} />
+                <Button title='Pay with Bank Transfer' onClick={() => console.log('clicked')} />
+                <Button2 className="bg-bk" title='Pay with NestPay' onClick={() => console.log('clicked')} />
+            </div>
+
+            <div id="step2">
+
+            </div>
+
         </div>
     )
 }
