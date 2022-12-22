93a19a46a5fbab0492e51d7bef90be5b6c232707
AbaniwoM <michaelabaniwo@gmail.com>
2022-12-22 03:01:56 +0100

    changes

--- src/Components/Inputs/Inputs.jsx
+++ src/Components/Inputs/Inputs.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "./Inputs.scss";
 
 // input component with props
 
 export const Inputs = ({ onChange, title, placeholder }) => {
     return (
-        <div>
+        <div className="input">
             <label>{title}</label>
             <input onChange={onChange} placeholder={placeholder} />
         </div>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Inputs } from '../Inputs/Inputs';
 import "./Login.scss";
 
 const Login = () => {
@@ -8,10 +9,17 @@ const Login = () => {
         <div className="head">Nest</div>
         <div className="content">
             <div className="left">
-                <h1>Welcome back</h1>
-                <h4>Login to your account</h4>
-                <div>
-                    
+                <div className="top">
+                  <h1>Welcome back</h1>
+                  <h4>Login to your account</h4>
+                </div>
+                <Inputs title="Email Address" onChange={""} placeholder="youremail@example.com" />
+                <Inputs title="Password" onChange={""} placeholder="*****************" />
+                <button>Login</button>
+                <div>Forgot password?</div>
+                <div className="create">
+                  <div>Are you a new user?</div>
+                  <div>Create an Account</div>
                 </div>
             </div>
             <div></div>
