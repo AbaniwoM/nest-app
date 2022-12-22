43a7d175b5d291faac9c2bafb6d73cd68513a66d
mo-renike <wilfriedakpe3@gmail.com>
2022-12-22 04:14:34 +0100

    Form steps

--- src/Components/Inputs/Inputs.jsx
+++ src/Components/Inputs/Inputs.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import arrow from "../../Assets/Stroke-1.svg"
-import bk from "../../Assets/bk.svg"
-import './Inputs.scss'
-import { Link } from 'react-router-dom'
-
-
-
+import React from "react";
+import arrow from "../../Assets/Stroke-1.svg";
+import bk from "../../Assets/bk.svg";
+import "./Inputs.scss";
 
 export const Inputs = ({ onChange, title, placeholder }) => {
     return (
-        <div>
+        <div className="input">
             <label>{title}</label>
             <input onChange={onChange} placeholder={placeholder} />
         </div>
-    )
-}
+    );
+};
 
 // button component with props and arrow icon
 
 export const Button = ({ title, onClick, className }) => {
     return (
-
-        <Link to="" className="btn" onClick={onClick}>{title} <img src={arrow} alt="" /> </Link>
-
-    )
-}
+        <button className="btn" onClick={onClick}>
+            {title} <img src={arrow} alt="" />{" "}
+        </button>
+    );
+};
 export const Button2 = ({ title, onClick, className }) => {
     return (
-        <Link className="btn bg-bk" to="/payment">{title}<img src={bk} alt="" /> </Link>
-
-
-    )
-}
+        <button onClick={onClick} className="btn bg-bk">
+            {title}
+            <img src={bk} alt="" />{" "}
+        </button>
+    );
+};
